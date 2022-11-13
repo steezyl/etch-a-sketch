9cc3e30822fe954f11d5aa5b274efb518c8c1f76
Stephanie McDonald <stephlmcdonald96@gmail.com>
2022-11-13 20:18:11 +1100

    Have changed grid size to custom entry and added a grid refresh

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const squares = 16; // default squares value 
+// const squares = 16; // default squares value 
 const gridContainer = document.querySelector('.square-grid-container');
+const gridSize = 400; //pixels
 
 // link to the form that will give the new squares value
 const numberSubmit = document.querySelector('.numberSubmit');
 const numberField = document.querySelector('.numberField');
 
+function hoverState() {
+    this.style.backgroundColor = "coral";
+}
+
 function createGrid () {
+const squares = Number(numberField.value);
+// clear old grid if any
+if (document.querySelector('.grid-item') !== null) {
+    const oldGrid = document.querySelectorAll('.grid-item');
+    oldGrid.forEach((element) => element.remove());
+}
 for (let i = 0; i<(squares+1); i++) {   // column counter
     // Create a row container for each row 
     const gridRowContainer = document.createElement('div');
@@ -15,8 +26,14 @@ for (let i = 0; i<(squares+1); i++) {   // column counter
         const gridItem = document.createElement('div');
         gridItem.classList.add(`column${j}`,'grid-item');
         gridRowContainer.appendChild(gridItem);
+        let gridWidth = (gridSize/squares);
+        gridItem.setAttribute("style", `width: ${gridWidth}px; height: ${gridWidth}px`);
     }
 }
+// Add hover listener event to all grid items
+const gridItem = document.querySelectorAll('.grid-item');
+gridItem.forEach(grid => grid.addEventListener('mouseover', hoverState));
+
 }
 
 numberSubmit.addEventListener('click', createGrid);
